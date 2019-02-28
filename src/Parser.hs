dc2dd5014af773e7266df83b669a357fbbfee8ca
Thomas Smith <tnsmith@live.nl>
2019-02-28 17:20:16 +0100

    Implement toString

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,5 +1,10 @@
 {-# Language OverloadedStrings #-}
-
+{-# Language GeneralizedNewtypeDeriving #-}
+{-# Language DeriveTraversable #-}
+{-# Language TypeSynonymInstances #-}
+{-# Language ScopedTypeVariables #-}
+{-# Language FlexibleInstances #-}
+{-# Language InstanceSigs #-}
 module Parser where
 
 import Data.Attoparsec.ByteString
@@ -7,15 +12,48 @@ import Data.Attoparsec.ByteString.Char8
 import Control.Applicative
 import qualified Data.ByteString.Char8 as C
 
-data Ast = SExpr [Ast] | SNum Int | SAtom String | SId String
-  deriving (Show, Eq)
+import Data.Functor.Foldable hiding (fold)
+import Data.Functor.Classes
+import Data.Functor.Compose
+
+import Data.Foldable
+
+import Data.Coerce (coerce)
+
+data AstF a = SExpr [a] | SNum Int | SAtom String | SId String
+  deriving (Show, Eq, Functor, Foldable, Traversable)
+
+type Ast = Fix AstF
+
+
+instance Eq1 AstF where 
+  liftEq :: forall a b. (a -> b -> Bool) -> AstF a -> AstF b -> Bool
+  liftEq f (SNum i) (SNum i') = i == i'
+  liftEq f (SAtom i) (SAtom i') = i == i'
+  liftEq f (SId i) (SId i') = i == i'
+  liftEq f (SExpr as) (SExpr bs) = and (zipWith f as bs)
+  liftEq f _ _ = False
+
+instance Show1 AstF where
+  liftShowsPrec f shwlst i s@(SNum n) = (\x -> ("(SNum " ++ show n ++ ")") ++ x) 
+  liftShowsPrec f shwlst i s@(SAtom n) = \x -> ("(SAtom " ++ n ++ ")") ++ x
+  liftShowsPrec f shwlst i s@(SId n) = \x -> ("(SId " ++ n ++ ")") ++ x
+  liftShowsPrec f shwlst i astF@(SExpr n) =   (\x -> "SExpr " ++ x) . shwlst n 
+
+    -- (\x -> "(SExpr " ++ x ++ ")") . fold q 
+    where q :: AstF ShowS = f i <$> astF
+
+sexpr = Fix . SExpr
+snum = Fix . SNum
+satom = Fix . SAtom
+sid = Fix . SId
 
 parseSexpr :: Parser Ast
 parseSexpr = (string  "(" >> innerList  <*  string ")") <|> num <|> atom <|> id
-  where innerList = SExpr <$> sepBy1 parseSexpr spaces
-        atom = lexeme $ SAtom <$> (char '\'' >> many1 letter_iso8859_15)
-        id   = lexeme $ SId <$> many1 letter_iso8859_15
-        num  = lexeme $ SNum <$> decimal
+  where innerList = (sexpr) <$> sepBy1 parseSexpr spaces
+        atom = lexeme $ (satom) <$> (char '\'' >> many1 letter_iso8859_15)
+        id   = lexeme $ (sid) <$> many1 letter_iso8859_15
+        num  = lexeme $ (snum) <$> decimal
 
 spaces = skipMany $ char ' '
 
--- test/ParserTests.hs
+++ test/ParserTests.hs
@@ -10,24 +10,24 @@ import qualified Data.ByteString.Char8 as C
 parserTests = describe "first test" $ do
                 it "parses parens" $
                   let
-                     expected = Just $ SExpr [SNum 5]
+                     expected = Just $ sexpr [snum 5]
                   in
                      maybeResult (parse parseSexpr "(5)") `shouldBe` expected
                 it "allows nested lists" $
                   let
-                     expected = Just $ SExpr [SExpr [SNum 5, SNum 6], SNum 6]
+                     expected = Just $ sexpr [sexpr [snum 5, snum 6], snum 6]
                   in maybeResult (parse parseSexpr "((5 6) 6)") `shouldBe` expected
                 it "allows atoms" $
                   let
-                     expected = Just $ SExpr [SExpr [SNum 5], SAtom "hoi"]
-                  in maybeResult (parse parseSexpr "((5) 'hoi)") `shouldBe` expected
+                     expected = Just $ sexpr [sexpr [snum 5], sexpr [snum 6], satom "hoi"]
+                  in maybeResult (parse parseSexpr "((5) (6) 'hoi)") `shouldBe` expected
                 it "allows ids" $
                   let
-                     expected = Just $ SExpr [SExpr [SNum 5], SId "hoi"]
-                  in maybeResult (parse parseSexpr "((5) hoi)") `shouldBe` expected
+                     expected = Just $ sexpr [sexpr [snum 5], sid "hoi"]
+                  in maybeResult (parse parseSexpr "((5) (6) hoi)") `shouldBe` expected
 
 lexerTests = describe "lexer tests" $ do
                 it "allows trailing white space" $
-                  maybeResult (parse parseSexpr "(5 )") `shouldBe` Just (SExpr [SNum 5])
+                  maybeResult (parse parseSexpr "(5 )") `shouldBe` Just (sexpr [snum 5])
                 it "allows heading white space" $
-                  maybeResult (parse parseSexpr "( 5)") `shouldBe` Just (SExpr [SNum 5])
+                  maybeResult (parse parseSexpr "( 5)") `shouldBe` Just (sexpr [snum 5])
